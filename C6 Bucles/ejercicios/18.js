34992948992ca16a5eafb7ec92e96a908edf953d
edgarmaestre <maestreedgarhernan@gmail.com>
2024-03-04 15:01:45 -0400

    Modulo 6 Terminado

--- C6 Bucles/ejercicios/18.js
+++ C6 Bucles/ejercicios/18.js
@@ -2,6 +2,13 @@ function productoEntreNúmeros(a, b) {
   // Dados dos argumentos "a" y "b", devuelve el producto de todos
   // los números entre a y b (inclusive).
   // Tu código:
+  if (a > b) {
+    return 0;
+  }
+  let producto = 1;
+  for (let i = a; i <= b; i++) {
+    producto *= i;
+  }
 }
 
 module.exports = productoEntreNúmeros;
--- C6 Bucles/ejercicios/25.js
+++ C6 Bucles/ejercicios/25.js
@@ -1,10 +1,13 @@
 function esPalindromo(string) {
-   // La función recibe un argumento "string".
-   // Verifica si este string es palíndromo o no.
-   // Retorna true si lo es, caso contrario, retorna false.
-   // IMPORTANTE: Un palíndromo es una palabra o frase
-   // que se lee igual hacia adelante que hacia atrás.
-   // Tu código:
+  // La función recibe un argumento "string".
+  // Verifica si este string es palíndromo o no.
+  // Retorna true si lo es, caso contrario, retorna false.
+  // IMPORTANTE: Un palíndromo es una palabra o frase
+  // que se lee igual hacia adelante que hacia atrás.
+  // Tu código:
+  let cleanString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+  let reversedString = cleanString.split("").reverse().join("");
+  return cleanString === reversedString;
 }
 
 module.exports = esPalindromo;
